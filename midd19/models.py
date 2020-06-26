7549ba783597f7d9164033e4cda2fd9a9347b598
Nestor Orozco <nestoreo@MacBook-Air.attlocal.net>
2020-06-26 12:14:35 -0700

    added comment and post model

--- midd19/models.py
+++ midd19/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import Users
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    anonymous = models.BooleanField(initial=False)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=model.CASCADE)
+    comment = models.CharField(max_length=1000)
+    anonymous = models.BooleanField(initial=False)
