2c9784caeec666a04737b9b432e8a81a4dd976d7
Nestor Orozco <nestoreo@MacBook-Air.attlocal.net>
2020-06-26 13:54:31 -0700

    added like and dislike fields

--- midd19/models.py
+++ midd19/models.py
@@ -8,9 +8,13 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
     anonymous = models.BooleanField(default=False)
+    likes=models.IntegerField(default=0)
+    dislikes=models.IntegerField(default=0)
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment = models.CharField(max_length=1000)
     anonymous = models.BooleanField(default=False)
+    likes=models.IntegerField(default=0)
+    dislikes=models.IntegerField(default=0)
