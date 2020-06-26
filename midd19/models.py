11da9de5993c19658f6098b9d94979cff39094d3
Nestor Orozco <nestoreo@MacBook-Air.attlocal.net>
2020-06-26 12:48:27 -0700

    added posts,comm to admin

--- midd19/models.py
+++ midd19/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import Users
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
-    anonymous = models.BooleanField(initial=False)
+    anonymous = models.BooleanField(default=False)
 
 class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=model.CASCADE)
     comment = models.CharField(max_length=1000)
-    anonymous = models.BooleanField(initial=False)
+    anonymous = models.BooleanField(default=False)
