6f41acaa696b841040cf3d708949e7aab0573ede
majd <majdh@middlebury.edu>
2020-07-01 11:25:02 -0400

    adding incomplete chatforum to master

--- midd19/urls.py
+++ midd19/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("chatforum", views.chatforum, name="chatforum"),
     path("user/<username>", views.user_view, name="user_view"),
-    path("user/<username>/<int:post_id>",views.post_view,name="post_view")
+    path("user/<username>/<int:post_id>",views.post_view,name="post_view"),
     path("like_dislike", views.like_dislike, name="like_dislike"),
 
 ]
